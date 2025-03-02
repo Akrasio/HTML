a9e928100c0611954ce9ba03c6d325f05e34f228
NightRaven <53287573+Xynnix@users.noreply.github.com>
2025-03-01 22:10:04 -0500

    Update vars.js

--- vars.js
+++ vars.js
@@ -1,7 +1,8 @@
-let streamer = window.location.pathname.substring(1)
+let streamer = window.location.search?.streamer.substring(1)
 let vips = window.location.search?.vips?.split(" ")
 let hex = window.location.search?.hex || "red"
 let settings = {
     hex,
     vips
 }
+module.exports = { streamer, settings }
